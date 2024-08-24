556ac00df7d1b2d30f146e8b4fa3a7ad3c68bb82
Benny Border <borderbenja@gmail.com>
2024-08-24 17:42:07 -0500

    i don't remember what this change is

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup, find_packages
 
+
 setup(
     name='futility',
     version='0.10',
@@ -10,10 +11,12 @@ setup(
             'fim=fim_scripts.fim:main',
         ],
     },
+
     package_data={
-        
-        'fim_scripts': ['default.cfg'],
+        'fim_scripts': ['default.cfg', '**/*'],
+        'futility': ['**/*.py', '**/*.xml', '**/*.fz']
     },
+
     install_requires=[
         'numpy',
         'matplotlib',
@@ -24,7 +27,7 @@ setup(
     ],
     author='Benny Border',
     author_email='borderbenja@gmail.com',
-    description='various .fits utilities',
+    description='various utilities for unpacking and analyzing .fits files',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/borderbenja05/futility',
